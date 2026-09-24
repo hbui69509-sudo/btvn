f5ee3f6beeee4558e8467ca313f1bb8445f1acf9
b25dctv022 Bùi Huy Hoàng <hbui69509@gmail.com>
2026-09-24 14:26:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const email = document.querySelector("#email");
 const err = document.querySelector("#email-error");
-const form = document.querySelector("#myForm"); // Đã khai báo biến form
+const form = document.querySelector("#myForm");
 
 function validateEmail() {
   const ok = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value);
@@ -12,11 +12,10 @@ function validateEmail() {
 email.addEventListener("input", validateEmail);
 
 form.addEventListener("submit", (e) => {
-  // Nếu email không hợp lệ, chặn việc gửi form
   if (!validateEmail()) {
     e.preventDefault(); 
   } else {
-    e.preventDefault(); // Chặn load lại trang để xem demo
+    e.preventDefault();
     alert("Email hợp lệ! Form đã được gửi.");
   }
 });
